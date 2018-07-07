59ef07943aff604bd6ad27a85e9b60a93ad9e9e6
pedromen9845 <40481627+pedromen9845@users.noreply.github.com>
2018-07-07 01:25:52 -0600

    Update Conect4.cpp

--- Conect4.cpp
+++ Conect4.cpp
@@ -301,8 +301,242 @@ int main(int argc, char const *argv[]) {
 
 
  void check (){
+  //filas
+ for (int i=0; i<=7; i++){
+	for (int j=0; j<=7;j++){
+			
+			if(tablero[i][j]==tablero[i][j+1] && tablero[i][j+2]==tablero[i][j+3] && tablero[i][j] == tablero[i][j+3] && tablero[i][j] !=0) {
+			kakuempate();
+			!endgame;}
+	}
+}
+ //columnas
+ for (int i=0; i<=7; i++){
+	for (int j=0; j<=7;j++){
+			
+			if(tablero[i][j]==tablero[i+1][j] && tablero[i+2][j]==tablero[i+3][j] && tablero[i][j] == tablero[i+3][j] && tablero[i][j] !=0) {
+			kakuempate();
+			!endgame;}
+	}
+}
+//diagonales izquierda a derecha
+if(tablero[0][4]==tablero[1][5] && tablero[2][6]==tablero[3][7] && tablero[0][4] == tablero[3][7] && tablero[0][4] !=0){
+			kakuempate();
+			!endgame;}
  
-    }
+else if(tablero[0][3]==tablero[1][4] && tablero[2][5]==tablero[3][6] && tablero[0][3] == tablero[3][6] && tablero[0][3] !=0){
+			kakuempate();
+			!endgame;}   
+ 
+else if(tablero[1][4]==tablero[2][5] && tablero[3][6]==tablero[4][7] && tablero[1][4] == tablero[4][7] && tablero[1][4] !=0){
+			kakuempate();
+			!endgame;}
+ 
+else if(tablero[0][2]==tablero[1][3] && tablero[2][4]==tablero[3][5] && tablero[0][2] == tablero[3][5] && tablero[0][2] !=0){
+			kakuempate();
+			!endgame;}
+ 
+else if(tablero[1][3]==tablero[2][4] && tablero[3][5]==tablero[4][6] && tablero[1][3] == tablero[4][6] && tablero[1][3] !=0){
+			kakuempate();
+			!endgame;}   
+
+else if(tablero[2][4]==tablero[3][5] && tablero[4][6]==tablero[5][7] && tablero[2][4] == tablero[5][7] && tablero[2][4] !=0){
+			kakuempate();
+			!endgame;}
+
+else if(tablero[0][1]==tablero[1][2] && tablero[2][3]==tablero[3][4] && tablero[0][1] == tablero[3][4] && tablero[0][1] !=0){
+			kakuempate();
+			!endgame;} 
+
+else if(tablero[1][2]==tablero[2][3] && tablero[3][4]==tablero[4][5] && tablero[1][2] == tablero[4][5] && tablero[1][2] !=0){
+			kakuempate();
+			!endgame;}  
+
+else if(tablero[2][3]==tablero[3][4] && tablero[4][5]==tablero[5][6] && tablero[2][3] == tablero[5][6] && tablero[2][3] !=0){
+			kakuempate();
+			!endgame;} 
+ 
+else if(tablero[3][4]==tablero[4][5] && tablero[5][6]==tablero[6][7] && tablero[3][4] == tablero[6][7] && tablero[3][4] !=0) {
+			kakuempate();
+			!endgame;}
+    
+else if(tablero[0][0]==tablero[1][1] && tablero[2][2]==tablero[3][3] && tablero[0][0] == tablero[3][3] && tablero[0][0] !=0){
+			kakuempate();
+			!endgame;}
+ 
+else if(tablero[1][1]==tablero[2][2] && tablero[3][3]==tablero[4][4] && tablero[1][1] == tablero[4][4] && tablero[1][1] !=0){
+			kakuempate();
+			!endgame;}   
+ 
+else if(tablero[2][2]==tablero[3][3] && tablero[4][4]==tablero[5][5] && tablero[2][2] == tablero[5][5] && tablero[2][2] !=0){
+			kakuempate();
+			!endgame;}
+ 
+else if(tablero[3][3]==tablero[4][4] && tablero[5][5]==tablero[6][6] && tablero[3][3] == tablero[6][6] && tablero[3][3] !=0){
+			kakuempate();
+			!endgame;}
+ 
+else if(tablero[4][4]==tablero[5][5] && tablero[6][6]==tablero[7][7] && tablero[4][4] == tablero[7][7] && tablero[4][4] !=0){
+			kakuempate();
+			!endgame;}   
+
+else if(tablero[2][4]==tablero[3][5] && tablero[4][6]==tablero[5][7] && tablero[2][4] == tablero[5][7] && tablero[2][4] !=0){
+			kakuempate();
+			!endgame;}
+
+else if(tablero[0][1]==tablero[1][2] && tablero[2][3]==tablero[3][4] && tablero[0][1] == tablero[3][4] && tablero[0][1] !=0){
+			kakuempate();
+			!endgame;} 
+
+else if(tablero[1][0]==tablero[2][1] && tablero[3][2]==tablero[4][3] && tablero[1][0] == tablero[4][3] && tablero[1][0] !=0){
+			kakuempate();
+			!endgame;}
+
+else if(tablero[2][1]==tablero[3][2] && tablero[4][3]==tablero[5][4] && tablero[2][1] == tablero[5][4] && tablero[2][1] !=0){
+			kakuempate();
+			!endgame;}
+ 
+else if(tablero[3][2]==tablero[4][3] && tablero[5][4]==tablero[6][5] && tablero[3][2] == tablero[6][5] && tablero[3][2] !=0){
+			kakuempate();
+			!endgame;}   
+ 
+else if(tablero[4][3]==tablero[5][4] && tablero[6][5]==tablero[7][6] && tablero[4][3] == tablero[7][6] && tablero[4][3] !=0){
+			kakuempate();
+			!endgame;}
+ 
+else if(tablero[2][0]==tablero[3][1] && tablero[4][2]==tablero[5][3] && tablero[2][0] == tablero[5][3] && tablero[2][0] !=0){
+			kakuempate();
+			!endgame;}
+ 
+else if(tablero[3][1]==tablero[4][2] && tablero[5][3]==tablero[6][4] && tablero[3][1] == tablero[6][4] && tablero[3][1] !=0){
+			kakuempate();
+			!endgame;}   
+
+else if(tablero[4][2]==tablero[5][3] && tablero[6][4]==tablero[7][5] && tablero[4][2] == tablero[7][5] && tablero[4][2] !=0){
+			kakuempate();
+			!endgame;}
+
+else if(tablero[3][0]==tablero[4][1] && tablero[5][2]==tablero[6][3] && tablero[3][0] == tablero[6][3] && tablero[3][0] !=0){
+			kakuempate();
+			!endgame;} 
+
+else if(tablero[4][1]==tablero[5][2] && tablero[6][3]==tablero[7][4] && tablero[4][1] == tablero[7][4] && tablero[4][1] !=0){
+			kakuempate();
+			!endgame;}  
+
+else if(tablero[4][0]==tablero[5][1] && tablero[6][2]==tablero[7][3] && tablero[4][0] == tablero[7][3] && tablero[4][0] !=0){
+			kakuempate();
+			!endgame;} 
+
+//diagonales de derecha a izquierda
+ 
+else if(tablero[7][4]==tablero[6][5] && tablero[5][6]==tablero[4][7] && tablero[7][4] == tablero[4][7] && tablero[7][4] !=0) {
+			kakuempate();
+			!endgame;}
+    
+else if(tablero[7][3]==tablero[6][4] && tablero[5][5]==tablero[4][6] && tablero[7][3] == tablero[4][6] && tablero[7][3] !=0){
+			kakuempate();
+			!endgame;}
+ 
+else if(tablero[6][4]==tablero[5][5] && tablero[4][6]==tablero[3][7] && tablero[6][4] == tablero[3][7] && tablero[6][4] !=0){
+			kakuempate();
+			!endgame;}   
+ 
+else if(tablero[7][2]==tablero[6][3] && tablero[5][4]==tablero[4][5] && tablero[7][2] == tablero[4][5] && tablero[7][2] !=0){
+			kakuempate();
+			!endgame;}
+ 
+else if(tablero[6][3]==tablero[5][4] && tablero[4][5]==tablero[3][6] && tablero[6][3] == tablero[3][6] && tablero[6][3] !=0){
+			kakuempate();
+			!endgame;}
+ 
+else if(tablero[5][4]==tablero[4][5] && tablero[3][6]==tablero[1][7] && tablero[5][4] == tablero[2][7] && tablero[4][4] !=0){
+			kakuempate();
+			!endgame;}   
+
+else if(tablero[7][1]==tablero[6][2] && tablero[5][3]==tablero[4][4] && tablero[7][1] == tablero[4][4] && tablero[7][1] !=0){
+			kakuempate();
+			!endgame;}
+
+else if(tablero[6][2]==tablero[5][3] && tablero[4][4]==tablero[3][5] && tablero[6][2] == tablero[3][5] && tablero[6][2] !=0){
+			kakuempate();
+			!endgame;} 
+
+else if(tablero[5][3]==tablero[4][4] && tablero[3][5]==tablero[2][6] && tablero[5][3] == tablero[2][6] && tablero[5][3] !=0){
+			kakuempate();
+			!endgame;}
+ 
+else if(tablero[4][4]==tablero[3][5] && tablero[2][6]==tablero[1][7] && tablero[4][4] == tablero[1][7] && tablero[4][4] !=0){
+			kakuempate();
+			!endgame;}
+ 
+else if(tablero[7][0]==tablero[6][1] && tablero[5][2]==tablero[4][3] && tablero[7][0] == tablero[4][3] && tablero[7][0] !=0) {
+			kakuempate();
+			!endgame;}
+    
+else if(tablero[6][1]==tablero[5][2] && tablero[4][3]==tablero[3][4] && tablero[6][1] == tablero[5][2] && tablero[6][1] !=0){
+			kakuempate();
+			!endgame;}
+ 
+else if(tablero[5][2]==tablero[4][3] && tablero[3][4]==tablero[2][5] && tablero[5][2] == tablero[2][5] && tablero[5][2] !=0){
+			kakuempate();
+			!endgame;}   
+ 
+else if(tablero[4][3]==tablero[3][4] && tablero[2][5]==tablero[1][6] && tablero[4][3] == tablero[1][6] && tablero[4][3] !=0){
+			kakuempate();
+			!endgame;}
+ 
+else if(tablero[3][4]==tablero[2][5] && tablero[1][6]==tablero[0][7] && tablero[3][4] == tablero[0][7] && tablero[3][4] !=0){
+			kakuempate();
+			!endgame;}
+ 
+else if(tablero[6][0]==tablero[5][1] && tablero[4][2]==tablero[3][3] && tablero[6][0] == tablero[3][3] && tablero[6][0] !=0){
+			kakuempate();
+			!endgame;}   
+
+else if(tablero[5][1]==tablero[4][2] && tablero[3][3]==tablero[2][4] && tablero[5][1] == tablero[2][4] && tablero[5][1] !=0){
+			kakuempate();
+			!endgame;}
+
+else if(tablero[4][2]==tablero[3][3] && tablero[2][4]==tablero[1][5] && tablero[4][2] == tablero[1][5] && tablero[4][2] !=0){
+			kakuempate();
+			!endgame;} 
+
+else if(tablero[3][3]==tablero[2][4] && tablero[1][5]==tablero[0][6] && tablero[3][3] == tablero[0][6] && tablero[3][3] !=0){
+			kakuempate();
+			!endgame;}
+ 
+else if(tablero[5][0]==tablero[4][1] && tablero[3][2]==tablero[2][3] && tablero[5][0] == tablero[2][3] && tablero[5][0] !=0){
+			kakuempate();
+			!endgame;}
+ 
+else if(tablero[4][1]==tablero[3][2] && tablero[2][3]==tablero[1][4] && tablero[4][1] == tablero[1][4] && tablero[4][1] !=0) {
+			kakuempate();
+			!endgame;}
+    
+else if(tablero[3][2]==tablero[2][3] && tablero[1][4]==tablero[0][5] && tablero[3][2] == tablero[0][5] && tablero[3][2] !=0){
+			kakuempate();
+			!endgame;}
+ 
+else if(tablero[4][0]==tablero[3][1] && tablero[2][2]==tablero[1][3] && tablero[4][0] == tablero[1][3] && tablero[4][0] !=0){
+			kakuempate();
+			!endgame;}   
+ 
+else if(tablero[3][1]==tablero[2][2] && tablero[1][3]==tablero[0][4] && tablero[3][1] == tablero[0][4] && tablero[3][1] !=0){
+			kakuempate();
+			!endgame;}
+ 
+else if(tablero[3][0]==tablero[2][1] && tablero[1][2]==tablero[0][3] && tablero[3][0] == tablero[0][3] && tablero[3][0] !=0){
+			kakuempate();
+			!endgame;}
+ 
+ 
+ 
+ 
+ //check final
+ }
+
+    
 
 void datos(){
 	
